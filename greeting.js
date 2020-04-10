7624632aecba41c8a1f36e34079ef815d7809040
caoimhe <caoimhe.gallahue@gmail.com>
2020-04-10 16:42:56 -0700

    finished other requirements

--- greeting.js
+++ greeting.js
@@ -1,12 +1,16 @@
-function greet(name){
-    if(name == ''){
-        return 'Hello, my friend';
+function greet(names){
+    if (Array.isArray(names)) {
+        newNames = parseNames(names);
+        return `Hello, ${group(newNames)}` + '.';
     }
-    if(uppercase(name)){
-        return 'HELLO ' + name + '!';
+    if(names == ''){
+        return 'Hello, my friend.';
+    }
+    if(uppercase(names)){
+        return 'HELLO ' + names + '!';
     }
     else{
-    return 'Hello, ' + name;
+    return 'Hello, ' + names + '.';
     }
 }
 
@@ -14,4 +18,46 @@ function uppercase(name){
     return name == name.toUpperCase();
 }
 
-module.exports = greet;
+function group(names){
+    if (names.length > 2){
+        newNames = oxfordComma(names);
+        return newNames.join('');
+    }
+    else{
+        return names.join(' and ');
+    }
+}
+
+function oxfordComma(names){
+    var namesWithCommas = [];
+    n = names.length - 1;
+    names.splice(n, 0, "and");
+    tot = names.length;
+    for(i = 0; i < tot; i++){
+        namesWithCommas.push(names[i]);
+        if(i < tot - 2){
+            namesWithCommas.push(', ');
+        }
+        else if((tot - i) > 1){
+            namesWithCommas.push(' ');
+        }
+    }
+    return namesWithCommas;
+}
+
+/*takes array of names and returns array of names split */
+function parseNames(names) {
+    return names.reduce((greetGroup, name) => {
+        if (name.indexOf('"') !== -1) {
+            greetGroup.push(name.replace(/"/g, ''))
+        } else if (name.indexOf(',') !== -1) {
+            greetGroup.push(...name.split(',').map(name => name.trim()));
+
+        } else {
+            greetGroup.push(name)
+        }
+        return greetGroup
+    }, [])
+}
+
+module.exports = {greet};
--- test.js
+++ test.js
@@ -1,20 +1,58 @@
-const greet = require('./greeting.js');
+const {greet} = require('./greeting.js');
 
+//Requirement 1
 describe('Greeting', () => {
     it('Tests the greeting', () => {
-      expect(greet("Tom")).toEqual("Hello, Tom");
-      expect(greet("Sally")).toEqual("Hello, Sally");
+      expect(greet("Tom")).toEqual("Hello, Tom.");
+      expect(greet("Sally")).toEqual("Hello, Sally.");
     });
   });
 
+  //Requirement 2
   describe('Null greeting', () => {
     it('Tests the greeting with null', () => {
-      expect(greet("")).toEqual("Hello, my friend");
+      expect(greet("")).toEqual("Hello, my friend.");
     });
   });
 
+  //Requirement 3
   describe('Shouting', () => {
     it('Tests the shouting greeting', () => {
       expect(greet("JERRY")).toEqual("HELLO JERRY!");
+      expect(greet("SAMUEL")).toEqual("HELLO SAMUEL!");
+    });
+  });
+
+  //Requirement 4
+  describe('Two people', () => {
+    it('Tests greeting with two people', () => {
+      expect(greet(["Jill", "Jane"])).toEqual("Hello, Jill and Jane.");
+      
+    });
+  });
+
+  //Requirement 5
+  describe('Oxford comma', () => {
+    it('Tests greeting with more than 2 people', () => {
+      expect(greet(["Amy", "Brian", "Charlotte"])).toEqual("Hello, Amy, Brian, and Charlotte.");
+      expect(greet(["Jack", "Jill", "Jane"])).toEqual("Hello, Jack, Jill, and Jane.");
+      
+    });
+  });
+
+
+ //Requirement 7
+ describe('String containing comma', () => {
+    it('Tests greeting with string split with a comma', () => {
+      expect(greet(["Bob", "Charlie, Dianne"])).toEqual("Hello, Bob, Charlie, and Dianne.");
+      
+    });
+  });
+
+  //Requirement 8
+  describe('Escape intentional commas', () => {
+    it('Tests greeting with string escaped commas', () => {
+      expect(greet(["Bob", "\"Charlie, Dianne\""])).toEqual("Hello, Bob and Charlie, Dianne.");
+      
     });
   });
